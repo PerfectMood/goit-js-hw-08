f3efb1eaefd05924c6bce3c6f12c70e5e8b2b480
Bohdan Frentser <bohdan.frentser@gmail.com>
2021-12-28 14:16:16 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+var throttle = require('lodash.throttle');
 
+const STORAGE_KEY = 'feedback-form-state';
+const formEl = document.querySelector('.feedback-form');
+
+const formData = {};
+reloadMemory();
+formEl.addEventListener('input', throttle(onSubmitFormInput, 500));
+
+function onSubmitFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+formEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function reloadMemory() {
+  const datalocalStorage = localStorage.getItem(STORAGE_KEY);
+  const parsedDatalocalStorage = JSON.parse(datalocalStorage);
+  if (parsedDatalocalStorage) {
+    Object.entries(parsedDatalocalStorage).forEach(([name, value]) => {
+      formEl.elements[name].value = value;
+      formData[name] = value;
+    });
+  }
+}
+console.log('Object', formData);
